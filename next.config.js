3de9091ee021475d72e2ec4c3ea31c27cee23fc6
Okkay914 <williambaumgarth@Gmail.com>
2024-11-26 23:47:58 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
+  output: 'export',
+  distDir: 'public', // Add this line
+  images: {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
